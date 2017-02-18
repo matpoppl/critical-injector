099478d4a372ba2efcfa969429d011729347abe1
Mateusz <mateusz@nc.com.pl>
2017-02-18 19:06:10 +0100

    Include _tmp in .gitignore

--- _tmp/test.js
+++
@@ -1,9 +0,0 @@
-const injector = require('./index.js');
-
-
-let actual = injector('test/_files/index.html', {
-    isPath: true,
-    basePath: __dirname + '/test/_files',
-});
-
-console.log(actual);
